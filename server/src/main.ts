78c2077ed214ab9f059c99cb6493d61830e9d1bf
JoDaeChan <daechan476@gmail.com>
2023-11-10 01:56:34 +0900

    :sparkles: Feat: createUser, login

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as dotenv from "dotenv";
 
 declare const module: any;
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
