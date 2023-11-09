a267487a41af983f87ae9c2f27437356264ff442
a-honey <jungahyeon0512@khu.ac.kr>
2023-11-09 15:02:23 +0900

    :sparkles: Feat: 절대 경로 설정

--- src/components/components/UserImg.tsx
+++ src/components/components/UserImg.tsx
@@ -1,4 +1,4 @@
-import user_none from '../../assets/user_none.png';
+import user_none from '@/assets/user_none.png';
 
 const UserImg = ({ src }: { src?: string }) => {
   return <img src={src ?? user_none} alt="" />;
