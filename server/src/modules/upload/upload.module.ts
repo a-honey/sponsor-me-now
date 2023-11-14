a55c3c5fa031d4eef0e2256900148b966b7fb90c
JoDaeChan <daechan476@gmail.com>
2023-11-15 06:11:33 +0900

    :hammer: Refactor: 오타..

--- server/src/modules/upload/upload.module.ts
+++ server/src/modules/upload/upload.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { MulterConfigModule } from "./multer.module";
 import { UploadService } from "./upload.service";
 import { UploadController } from "./upload.controller";
-import { PrismaClient } from "@prisma/Client";
+import { PrismaClient } from "@prisma/client";
 
 @Module({
   imports: [MulterConfigModule],
