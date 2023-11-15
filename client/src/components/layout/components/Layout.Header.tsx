12f3a942825982de0e1c6c50edf4f699710e17e9
a-honey <jungahyeon0512@khu.ac.kr>
2023-11-16 00:56:11 +0900

    :sparkles: Design: 헤더 반응형 추가

--- client/src/components/layout/components/Layout.Header.tsx
+++ client/src/components/layout/components/Layout.Header.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import styles from '../styles/Layout.Header.module.scss';
 import { RxHamburgerMenu, RxBell } from 'react-icons/rx';
 import Nav from './Layout.Nav';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [isOpenNav, setIsOpenNav] = useState(false);
@@ -16,9 +17,19 @@ const Header = () => {
         <div className={styles.logo}>
           <div>Sponsor Me Now</div>
         </div>
+        <div className={styles.links}>
+          <Link to="/main">Home</Link>
+          <Link to="/hub">Hub</Link>
+          <Link to="/list">My Sponsor</Link>
+          <Link to="/mypage">My Page</Link>
+          <Link to="/payment">Payment History</Link>
+        </div>
         <div className={styles.icons}>
           <RxBell>알림</RxBell>
           <RxHamburgerMenu onClick={toggleIsOpenNav}>목록</RxHamburgerMenu>
+        </div>
+        <div className={styles.pcIcons}>
+          <div>로그아웃</div>
         </div>
       </header>
       {isOpenNav && <Nav toggleIsOpenNav={toggleIsOpenNav} />}
