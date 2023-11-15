f1092c60f43abf4615ebdd84e82036c208ac59da
a-honey <jungahyeon0512@khu.ac.kr>
2023-11-15 22:27:50 +0900

    :lipstick: Design: section, article 레이아웃 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />;
+      <Router />
     </QueryClientProvider>
   );
 }
--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,17 +1,13 @@
 import Header from './components/Layout.Header';
-import LeftBox from './components/Layout.LeftBox';
-import RightBox from './components/Layout.RightBox';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <main>
-      <LeftBox />
       <section>
         <Header />
         <Outlet />
       </section>
-      <RightBox />
     </main>
   );
 };
