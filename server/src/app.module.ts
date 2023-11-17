2056ad8b950979476f08f754ea0e4f12cf0df136
JoDaeChan <daechan476@gmail.com>
2023-11-17 11:05:05 +0900

    :hammer: Refactor: 업로드 prefix 수정 / public 개방

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,8 +5,18 @@ import { UploadModule } from "./modules/upload/upload.module";
 import { PostModule } from "./modules/post/post.module";
 import { LikeModule } from "./modules/like/like.module";
 import { CommentModule } from "./modules/comment/comment.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
-  imports: [AuthModule, UserModule, UploadModule, PostModule, LikeModule, CommentModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    UploadModule,
+    PostModule,
+    LikeModule,
+    CommentModule,
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, "..", "public") }),
+  ],
 })
 export class AppModule {}
