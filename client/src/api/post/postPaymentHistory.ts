dbe291fb23ee08fdb684f577e61b930cb8014cd9
a-honey <jungahyeon0512@khu.ac.kr>
2023-11-20 06:28:47 +0900

    :sparkles: Feat: 후원 금액 선택 모달 추가

--- client/src/api/post/postPaymentHistory.ts
+++ client/src/api/post/postPaymentHistory.ts
@@ -1,36 +1,31 @@
 import { instance } from '../instance';
 
-export interface PostBodyType {
-  applyNum: string;
-  bankCode: string;
-  bankName: string;
-  buyerAddr: string;
-  buyerEmail: string;
-  buyerName: string;
-  buyerPostcode: string;
-  buyerTel: string;
-  cardName: string;
-  cardQuota: number;
-  currency: string;
-  customData: string;
-  escrow: boolean;
-  failReason: string;
-  impUid: string;
-  merchantUid: string;
-  name: string;
-  paidAmount: number;
-  paidAt: number;
-  payMethod: string;
-  pgId: string;
-  pgProvider: string;
-  pgTid: string;
-  receiptUrl: string;
-  status: string;
+export interface PostPaymentHistoryResponse {
+  applyNum?: number;
+  bankName?: string;
+  buyerAddr?: string;
+  buyerEmail?: string;
+  buyerName?: string;
   sellerName: string;
   sellerEmail: string;
+  buyerPostcode?: string;
+  buyerTel?: string;
+  cardName?: string;
+  cardQuota?: number;
+  customData?: string;
+  impUid?: string;
+  merchantUid?: string;
+  name?: string;
+  paidAmount?: number;
+  paidAt?: string;
+  payMethod?: string;
+  pgProvider?: string;
+  pgTid?: string;
+  receiptUrl?: string;
+  status?: string;
 }
 
-const postPaymentHistory = async (body: any) => {
+const postPaymentHistory = async (body: PostPaymentHistoryResponse) => {
   try {
     const response = await instance.post('/payments/complete', body);
     return response.data;
--- client/src/pages/userId/components/UserId.UserHeader.tsx
+++ client/src/pages/userId/components/UserId.UserHeader.tsx
@@ -1,6 +1,6 @@
-import { useLoginStore } from '@/store';
 import styles from '../styles/UserId.UserHeader.module.scss';
-import postPaymentHistory from '@/api/post/postPaymentHistory';
+import PaymentSelect from './UserId.PaymentSelect';
+import { useState } from 'react';
 
 const UserHeader = ({
   data,
@@ -13,143 +13,41 @@ const UserHeader = ({
     field: string;
   };
 }) => {
+  const [isOpenPaymentSelect, setIsOpenPaymentSelect] = useState(false);
   const { id, username, email, description, field } = data;
+
+  const toggleIsOpenPaymentSelect = () => {
+    setIsOpenPaymentSelect((prev) => !prev);
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.contentContainer}>
-        <div className={styles.item}>
-          <label>이름</label>
-          <div>{username}</div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.contentContainer}>
+          <div className={styles.item}>
+            <label>이름</label>
+            <div>{username}</div>
+          </div>
+          <div className={styles.item}>
+            <label>분야</label>
+            <div>{field}</div>
+          </div>
+          <div className={styles.item}>
+            <label>소개</label>
+            <div>{description}</div>
+          </div>
         </div>
-        <div className={styles.item}>
-          <label>분야</label>
-          <div>{field}</div>
-        </div>
-        <div className={styles.item}>
-          <label>소개</label>
-          <div>{description}</div>
-        </div>
+        <button onClick={toggleIsOpenPaymentSelect}>후원하기</button>
       </div>
-      <RequestPay email={email} username={username} id={id} />
-    </div>
+      {isOpenPaymentSelect && (
+        <PaymentSelect
+          toggleIsOpenPaymentSelect={toggleIsOpenPaymentSelect}
+          email={email}
+          username={username}
+          id={id}
+        />
+      )}
+    </>
   );
 };
 
 export default UserHeader;
-
-interface Response {
-  apply_num?: number;
-  bank_name?: string;
-  buyer_addr?: string;
-  buyer_email?: string;
-  buyer_name?: string;
-  buyer_postcode?: string;
-  buyer_tel?: string;
-  card_name?: string;
-  card_quota?: number;
-  custom_data?: string;
-  imp_uid?: string;
-  merchant_uid?: string;
-  name?: string;
-  paid_amount?: number;
-  paid_at?: string;
-  pay_method?: string;
-  pg_provider?: string;
-  pg_tid?: string;
-  receipt_url?: string;
-  status?: string;
-  success: boolean;
-}
-
-interface RequestPayParams {
-  pg: string;
-  pay_method: string;
-  merchant_uid: string;
-  name: string;
-  amount: number;
-  buyer_email: string;
-  buyer_name: string;
-  buyer_tel: string;
-  buyer_addr: string;
-  buyer_postcode: string;
-}
-
-const RequestPay = ({
-  email,
-  username,
-}: {
-  email: string;
-  username: string;
-  id: number;
-}) => {
-  const code = import.meta.env.VITE_IAMPORT_CODE;
-
-  const { loginUsername, loginEmail } = useLoginStore();
-
-  if (!window.IMP || !code) return;
-  const { IMP } = window;
-  IMP.init(code);
-
-  const today = new Date();
-  const hours = today.getHours(); // 시
-  const minutes = today.getMinutes(); // 분
-  const seconds = today.getSeconds(); // 초
-  const milliseconds = today.getMilliseconds();
-  const makeMerchantUid = hours + minutes + seconds + milliseconds;
-  const requestPay = () => {
-    (
-      IMP.request_pay as (
-        params: RequestPayParams,
-        callback: (rsp: Response) => void,
-      ) => void
-    )(
-      {
-        pg: `kakaopay.${import.meta.env.VITE_IAMPORT_KAKAOPAY_ID!}`,
-        pay_method: 'card',
-        merchant_uid: `IMP_${makeMerchantUid}`,
-        name: `${loginUsername}의 ${username} 후원`,
-        amount: 1000,
-        buyer_email: loginEmail!,
-        buyer_name: loginUsername!,
-        buyer_tel: '010-4242-4242',
-        buyer_addr: '서울특별시 강남구 신사동',
-        buyer_postcode: '01181',
-      },
-      (rsp: Response) => {
-        if (rsp.success) {
-          console.log('결제성공', rsp);
-          postPaymentHistory({
-            applyNum: rsp.apply_num,
-            bankName: rsp.bank_name,
-            buyerAddr: rsp.buyer_addr,
-            buyerEmail: rsp.buyer_email,
-            buyerName: rsp.buyer_name,
-            sellerName: username,
-            sellerEmail: email,
-            buyerPostcode: rsp.buyer_postcode,
-            buyerTel: rsp.buyer_tel,
-            cardName: rsp.card_name,
-            cardQuota: rsp.card_quota,
-            customData: rsp.custom_data,
-            impUid: rsp.imp_uid,
-            merchantUid: rsp.merchant_uid,
-            name: rsp.name,
-            paidAmount: rsp.paid_amount,
-            paidAt: rsp.paid_at,
-            payMethod: rsp.pay_method,
-            pgProvider: rsp.pg_provider,
-            pgTid: rsp.pg_tid,
-            receiptUrl: rsp.receipt_url,
-            status: rsp.status,
-          });
-        } else {
-          console.log('결제실패', rsp);
-          // 결제 실패 시 로직
-          // ...
-        }
-      },
-    );
-  };
-
-  return <button onClick={requestPay}>후원하기</button>;
-};
