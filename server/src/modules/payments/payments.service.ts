c95cd66729c248260b4a2759d517ba78f15ff3a2
JoDaeChan <daechan476@gmail.com>
2023-11-21 02:24:30 +0900

    :hammer: Refactor: createPaymentsHistory API
    결제내역 생성시 user 레코드에 account 증감
    AccountHistory 관계테이블로 입출금 내역 저장

--- server/src/modules/payments/payments.service.ts
+++ server/src/modules/payments/payments.service.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { ImpUidDto } from "./dto/impUid.dto";
 import { PaymentsDataDto } from "./dto/paymentsDataDto";
 import { ResponsePaymentHistoryDto } from "./dto/responsePaymentHistory.dto";
-import { plainToClass } from "class-transformer";
+import { plainToInstance } from "class-transformer";
 
 @Injectable()
 export class PaymentsService {
@@ -14,59 +14,84 @@ export class PaymentsService {
     data: ImpUidDto,
     paymentsData: PaymentsDataDto,
   ): Promise<ResponsePaymentHistoryDto> {
-    const createdPayments = this.prisma.payment.create({
-      data: {
-        buyerId: userId,
-        sellerEmail: data.sellerEmail,
-        sellerName: data.sellerName,
-        amount: paymentsData.amount,
-        applyNum: paymentsData.apply_num,
-        bankCode: paymentsData.bank_code,
-        bankName: paymentsData.bank_name,
-        buyerAddr: paymentsData.buyer_addr,
-        buyerEmail: paymentsData.buyer_email,
-        buyerName: paymentsData.buyer_name,
-        buyerPostcode: paymentsData.buyer_postcode,
-        buyerTel: paymentsData.buyer_tel,
-        cancelAmount: paymentsData.cancel_amount,
-        cancelReason: paymentsData.cancel_reason,
-        cancelledAt: paymentsData.cancelled_at,
-        cardCode: paymentsData.card_code,
-        cardName: paymentsData.card_name,
-        cardNumber: paymentsData.card_number,
-        cardQuota: paymentsData.card_quota,
-        cardType: paymentsData.card_type,
-        cashReceiptIssued: paymentsData.cash_receipt_issued,
-        channel: paymentsData.channel,
-        currency: paymentsData.currency,
-        customData: paymentsData.custom_data,
-        customerUid: paymentsData.customer_uid,
-        customerUidUsage: paymentsData.customer_uid_usage,
-        embPgProvider: paymentsData.emb_pg_provider,
-        escrow: paymentsData.escrow,
-        failReason: paymentsData.fail_reason,
-        failedAt: paymentsData.failed_at,
-        impUid: paymentsData.imp_uid,
-        merchantUid: paymentsData.merchant_uid,
-        name: paymentsData.name,
-        paidAt: paymentsData.paid_at,
-        payMethod: paymentsData.pay_method,
-        pgId: paymentsData.pg_id,
-        pgProvider: paymentsData.pg_provider,
-        pgTid: paymentsData.pg_tid,
-        receiptUrl: paymentsData.receipt_url,
-        startedAt: paymentsData.started_at,
-        status: paymentsData.status,
-        userAgent: paymentsData.user_agent,
-        vbankCode: paymentsData.vbank_code,
-        vbankDate: paymentsData.vbank_date,
-        vbankHolder: paymentsData.vbank_holder,
-        vbankIssuedAt: paymentsData.vbank_issued_at,
-        vbankName: paymentsData.vbank_name,
-        vbankNum: paymentsData.vbank_num,
-      },
+    const result = await this.prisma.$transaction(async (prisma) => {
+      const updatedUser = await this.prisma.user.update({
+        where: { id: userId },
+        data: {
+          account: {
+            increment: paymentsData.amount,
+          },
+        },
+      });
+
+      const createdPayments = await this.prisma.payment.create({
+        data: {
+          buyerId: userId,
+          sellerEmail: data.sellerEmail,
+          sellerName: data.sellerName,
+          amount: paymentsData.amount,
+          applyNum: paymentsData.apply_num,
+          bankCode: paymentsData.bank_code,
+          bankName: paymentsData.bank_name,
+          buyerAddr: paymentsData.buyer_addr,
+          buyerEmail: paymentsData.buyer_email,
+          buyerName: paymentsData.buyer_name,
+          buyerPostcode: paymentsData.buyer_postcode,
+          buyerTel: paymentsData.buyer_tel,
+          cancelAmount: paymentsData.cancel_amount,
+          cancelReason: paymentsData.cancel_reason,
+          cancelledAt: paymentsData.cancelled_at,
+          cardCode: paymentsData.card_code,
+          cardName: paymentsData.card_name,
+          cardNumber: paymentsData.card_number,
+          cardQuota: paymentsData.card_quota,
+          cardType: paymentsData.card_type,
+          cashReceiptIssued: paymentsData.cash_receipt_issued,
+          channel: paymentsData.channel,
+          currency: paymentsData.currency,
+          customData: paymentsData.custom_data,
+          customerUid: paymentsData.customer_uid,
+          customerUidUsage: paymentsData.customer_uid_usage,
+          embPgProvider: paymentsData.emb_pg_provider,
+          escrow: paymentsData.escrow,
+          failReason: paymentsData.fail_reason,
+          failedAt: paymentsData.failed_at,
+          impUid: paymentsData.imp_uid,
+          merchantUid: paymentsData.merchant_uid,
+          name: paymentsData.name,
+          paidAt: paymentsData.paid_at,
+          payMethod: paymentsData.pay_method,
+          pgId: paymentsData.pg_id,
+          pgProvider: paymentsData.pg_provider,
+          pgTid: paymentsData.pg_tid,
+          receiptUrl: paymentsData.receipt_url,
+          startedAt: paymentsData.started_at,
+          status: paymentsData.status,
+          userAgent: paymentsData.user_agent,
+          vbankCode: paymentsData.vbank_code,
+          vbankDate: paymentsData.vbank_date,
+          vbankHolder: paymentsData.vbank_holder,
+          vbankIssuedAt: paymentsData.vbank_issued_at,
+          vbankName: paymentsData.vbank_name,
+          vbankNum: paymentsData.vbank_num,
+        },
+      });
+
+      await this.prisma.accountHistory.create({
+        data: {
+          withdrawnAmount: paymentsData.amount,
+          remainingAmount: updatedUser.account,
+          bank: paymentsData.bank_name,
+          accountNumber: paymentsData.vbank_num,
+          transactionType: "DEPOSIT",
+          user: {
+            connect: { id: userId },
+          },
+        },
+      });
+      return createdPayments;
     });
 
-    return plainToClass(ResponsePaymentHistoryDto, createdPayments);
+    return plainToInstance(ResponsePaymentHistoryDto, result);
   }
 }
