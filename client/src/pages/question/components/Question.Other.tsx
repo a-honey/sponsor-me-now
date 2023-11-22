a08d032fc3cc7b9c1ffac76c7e35148de569e6af
a-honey <jungahyeon0512@khu.ac.kr>
2023-11-22 09:17:19 +0900

    :sparkles: Feat: emailjs를 통한 문의 남기기

--- client/src/pages/question/components/Question.Other.tsx
+++ client/src/pages/question/components/Question.Other.tsx
@@ -1,11 +1,51 @@
+import emailjs from '@emailjs/browser';
+import { useForm } from 'react-hook-form';
+
+interface EmailBodyType {
+  name: string;
+  email: string;
+  title: string;
+  message: string;
+}
+
 const Other = () => {
+  const { register, handleSubmit, setValue } = useForm<EmailBodyType>();
+  const onSubmit = async (data: EmailBodyType) => {
+    const response = await emailjs.send(
+      import.meta.env.VITE_EMAIL_SERVICE_KEY,
+      import.meta.env.VITE_EMAIL_TEMPLATE_KEY,
+      {
+        name: data.name,
+        email: data.email,
+        title: data.title,
+        message: data.message,
+      },
+      import.meta.env.VITE_EMAIL_PUBLIC_KEY,
+    );
+
+    if (response.status === 200) {
+      setValue('name', data.name);
+      setValue('email', data.email);
+      setValue('title', data.title);
+      setValue('message', data.message);
+    }
+  };
   return (
-    <form>
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
       <h4>직접 문의하기</h4>
       <label>작성자</label>
-      <input />
+      <input type="text" {...register('name')} />
+      <label>이메일</label>
+      <input
+        placeholder="답변받으실 이메일을 입력해주세요"
+        type="text"
+        {...register('email')}
+      />
+      <label>문의 제목</label>
+      <input type="text" {...register('title')} />
       <label>문의 내용</label>
-      <textarea />
+      <input type="text" {...register('message')} />
+      <button>제출</button>
     </form>
   );
 };
