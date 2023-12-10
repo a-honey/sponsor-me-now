b1a6acb2f6ca5cbe2e83b9d889926f6feee2be04
JoDaeChan <daechan476@gmail.com>
2023-12-10 16:37:47 +0900

    :ambulance: Fix: upload module 종속성 변경
    프리즈마 클리이언트 삭제
    config service 추가

--- server/src/modules/upload/upload.module.ts
+++ server/src/modules/upload/upload.module.ts
@@ -2,14 +2,14 @@ import { Module } from "@nestjs/common";
 import { MulterConfigModule } from "./multer.module";
 import { UploadService } from "./upload.service";
 import { UploadController } from "./upload.controller";
-import { PrismaClient } from "@prisma/client";
+import { ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../../entitys/user";
 import { Post } from "../../entitys/post";
 
 @Module({
   imports: [MulterConfigModule, TypeOrmModule.forFeature([User, Post])],
-  providers: [UploadService, PrismaClient],
+  providers: [UploadService, ConfigService],
   controllers: [UploadController],
 })
 export class UploadModule {}
